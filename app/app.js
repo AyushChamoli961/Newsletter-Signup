117b15c63b85d689a361d84971d6ad1881322caa
Ayush Chamoli <94229463+AyushChamoli961@users.noreply.github.com>
2022-08-05 01:21:00 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -27,7 +27,3 @@ app.post("/" ,function(req,res){
 app.listen(3000 , function(req,res){
     console.log("Server is running at port 3000")
 })
-
-//api 2c3b29801de861bb7433bd294958d038-us8
-
-//audience id a138bbc83c
